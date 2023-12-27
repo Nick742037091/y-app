f7f64d6678564007aca6f00ef73a42b93863fc41
nick <742037091@qq.com>
2023-12-27 11:21:20 +0800

    ✨ feat(request):
    chang mock api host

--- src/services/request/instances/y.ts
+++ src/services/request/instances/y.ts
@@ -1,7 +1,7 @@
 import { createInstance } from '../base/index'
 
 export const yApi = createInstance({
-  baseUrl: 'http://127.0.0.1:4523/m1/3795937-0-default',
+  baseUrl: 'https://mock.apifox.com/m1/3795937-0-default',
   options: {
     timeout: 10000 // 默认超时10s
   }
