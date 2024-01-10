a936d07885af83706a0982a4efd1c695bf2e6931
nick <742037091@qq.com>
2024-01-10 11:50:51 +0800

    ✨ feat(home): 增加首页gif播放功能

--- src/pages/index/components/PostMedia/index.tsx
+++ src/pages/index/components/PostMedia/index.tsx
@@ -1,10 +1,11 @@
-import Taro from '@tarojs/taro'
-import { Video, View } from '@tarojs/components'
+import Taro, { VideoContext } from '@tarojs/taro'
+import { Image, Video, View } from '@tarojs/components'
 import { PostItem } from '@/services/post/types'
 import { playVideo } from '@/utils'
 import IconFont from '@/components/IconFont/index'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { uniqueId } from 'lodash'
 import styles from './index.module.scss'
-import { useState } from 'react'
 
 const createImage = (
   className: string,
@@ -94,44 +95,68 @@ export const VideoPoster = (props: { video: string; poster: string }) => {
 }
 
 export const GifPoster = (props: { gifVideo: string; poster: string }) => {
+  /** 中间按钮 **/
   const [isToggled, setToggled] = useState(false)
-  const handleToggle = () => {
-    setToggled(true)
-    setIsPlaying(true)
-  }
   const centerPlayButton = (
-    <View className={styles.gif_play_btn} onClick={handleToggle}>
+    <View className={styles.gif_play_btn} onClick={() => setToggled(true)}>
       <IconFont name="play" size={30} color="white" />
     </View>
   )
+  /** 播放状态 **/
   const [isPlaying, setIsPlaying] = useState(false)
+  const togglePlay = () => {
+    const nextIsPlaying = !isPlaying
+    nextIsPlaying ? videoContext.current?.play() : videoContext.current?.pause()
+    setIsPlaying(nextIsPlaying)
+  }
+  /** 底部播放按钮 **/
   const bottomPlayButton = (
     <View className={styles.gif_bottom_bar}>
-      <View
-        className={styles.gif_bottom_play_button}
-        onClick={() => setIsPlaying(!isPlaying)}
-      >
+      <View className={styles.gif_bottom_play_button} onClick={togglePlay}>
         <IconFont name={isPlaying ? 'pause' : 'play'} size={16} color="white" />
       </View>
       <View className={styles.gif_tag}>GIF</View>
     </View>
   )
+  /* 重新render不会替换video元素
+   * videoId只生成一次即可，尽管videoId变化不会导致video元素重新渲染
+   */
+  const videoId = useRef(`video-player-${uniqueId()}`)
+  const videoContext = useRef<VideoContext | null>()
+  // 是否已加载视频
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false)
+  const handleLoadedMetaData = () => {
+    videoContext.current = Taro.createVideoContext(videoId.current, this)
+    setIsVideoLoaded(true)
+    // 因autoplay需要更新播放状态
+    setIsPlaying(true)
+  }
   return (
     // padding-bottom控制整体宽高比
     <View className="pb-[50%] relative mt-5">
       <View className="absolute-full">
         <View className={styles.img_row + ' h-full'}>
-          <Video
-            src={props.gifVideo}
-            className={styles.gif_video + ' w-full h-full bg-placeholder'}
-            controls={false}
-            showProgress={false}
-            showPlayBtn={false}
-            showCenterPlayBtn={false}
-            showMuteBtn={false}
-            showFullscreenBtn={false}
-            showBottomProgress={false}
-          />
+          {/* 点击之后才加载视频，避免页面渲染时批量预加载视频 */}
+          {isToggled && (
+            <Video
+              id={videoId.current}
+              loop
+              autoplay
+              src={props.gifVideo}
+              className={styles.gif_video + ' w-full h-full'}
+              controls={false}
+              showCenterPlayBtn={false}
+              onLoadedMetaData={handleLoadedMetaData}
+            />
+          )}
+          {/* 默认展示封面，视频加载之后隐藏封面 */}
+          {/* TODO 可增加视频加载时的loading */}
+          {isVideoLoaded || (
+            <Image
+              src={props.poster}
+              className="absolute-full w-full h-full z-[1] bg-placeholder"
+            />
+          )}
           {isToggled ? bottomPlayButton : centerPlayButton}
         </View>
       </View>
@@ -139,7 +164,7 @@ export const GifPoster = (props: { gifVideo: string; poster: string }) => {
   )
 }
 
-const Media = (props: { post: PostItem }) => {
+const Media = (props: { post: PostItem; index: number }) => {
   const { post } = props
   if (post.video) {
     return <VideoPoster video={post.video} poster={post.videoPoster} />
