41cdd72b83097cd2bfe368a668607ae7afdbed2c
nick <742037091@qq.com>
2024-01-11 23:32:12 +0800

    🐞 fix(home): 修复首页导航栏回显过快的问题

--- src/components/ImmersionNavigation/index.tsx
+++ src/components/ImmersionNavigation/index.tsx
@@ -18,7 +18,7 @@ const ImmersionTop = (props: {
   footerHeight?: number
 }) => {
   // 若状态栏文字颜色为白色，需要设置背景颜色，透明度用于实现玻璃效果，
-  const backgroundColor = props.backgroundColor || 'rgba(255,255,255,0.85)'
+  const backgroundColor = props.backgroundColor || 'rgba(255,255,255)'
   const { statusBarHeight, statusBarPosition } = useStatusBarHeight({
     backgroundColor
   })
@@ -38,27 +38,29 @@ const ImmersionTop = (props: {
   const scrollTop = useRef(0)
   const SHOW_SCROLL_DELTA_Y = 30
   // 导航栏透明度
-  const navigationOpacity =
-    (navigationHeight - navigationTranslateY) / navigationHeight
+  const navigationOpacity = (totalHeight - navigationTranslateY) / totalHeight
   // 监听页面滚动，根据滚动距离，设置导航栏的transform: translateY()
   usePageScroll((info) => {
     const deltaY = info.scrollTop - scrollTop.current
     if (deltaY > 0) {
-      // 向下滚动，逐渐隐藏导航栏，位移距离不能大于导航栏高度
+      // 向下滚动，逐渐隐藏导航栏，位移距离不能大于导航栏高度，避免回显动效过快
+      // 可能在页面中间显示导航栏，所以不能设置为scrollTop
       setNavigationTranslateY(
-        Math.min(navigationTranslateY + deltaY, navigationHeight)
+        Math.min(navigationTranslateY + deltaY, totalHeight)
       )
     } else {
       // 向上滚动
-      if (scrollTop.current > navigationHeight) {
+      if (scrollTop.current > totalHeight) {
         // 当前滚动距离超过导航栏高度，快速滚动才会显示导航栏
         if (deltaY < -SHOW_SCROLL_DELTA_Y) {
           // 单次向上滚动距离超过30px，就显示导航栏
           setNavigationTranslateY(0)
         }
       } else {
-        // 滚动到顶部，逐渐显示导航栏，位移距离不能小于0
-        setNavigationTranslateY(Math.min(navigationTranslateY + deltaY, 0))
+        // 未回显完整导航栏时，逐渐展示导航栏，因为滚动到顶，设置为scrollTop即可
+        if (navigationTranslateY > 0) {
+          setNavigationTranslateY(info.scrollTop)
+        }
       }
     }
     scrollTop.current = info.scrollTop
@@ -69,7 +71,8 @@ const ImmersionTop = (props: {
     height: navigationHeight + footerHeight + 'px',
     // 通过transform: translateY()将导航栏移动到状态栏下方，并设置transform动画
     transform: `translateY(-${navigationTranslateY}px)`,
-    opacity: navigationOpacity,
+    // 最小透明度不为零，避免滚动过快，瞬间透明不可见
+    opacity: Math.min(navigationOpacity + 0.2, 1),
     transition: `all ${transitionTime}ms ease-out`,
     backgroundColor
   }
@@ -89,18 +92,6 @@ const ImmersionTop = (props: {
       <View style={{ height: totalHeight + 'px' }} />
     </>
   )
-}
-
-// 状态栏高度占位，固定布局，层级在导航栏之上，避免导航栏遮挡，不会覆盖系统状态信息
-const StatusBarPosition = (props: {
-  height: number
-  backgroundColor: string
-}) => {
-  const style: CSSProperties = {
-    height: props.height + 'px',
-    backgroundColor: props.backgroundColor
-  }
-  return <View className="fixed left-0 right-0 top-0 z-[2]" style={style} />
 }
 
 export default ImmersionTop
