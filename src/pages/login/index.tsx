b4833cac12bcd6de3f6be977710f93da80670035
nick <742037091@qq.com>
2025-07-27 16:45:11 +0800

    ✨ feat: 增加登录埋点

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -53,6 +53,9 @@ export default function Index() {
   const appStore = useAppStore()
 
   const handleLogin = async () => {
+    umami.trackEvent('login', {
+      userName
+    })
     if (!userName) {
       showError('请输入账号')
       return
